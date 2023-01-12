06e112fa6048b8c44b8677eb6e601ac97c8ab96b
Onyeka Obi <Onyeka.Obi@gmail.com>
2023-01-12 15:04:10 -0800

    Fix hash stuff

--- src/tree/hash.rs
+++ src/tree/hash.rs
@@ -1,8 +1,4 @@
-use sha2::{Digest, Sha512_256};
 use std::convert::TryFrom;
-
-/// The hash algorithm used for both KV hashes and node hashes.
-pub type Hasher = Sha512_256;
 
 /// The length of a `Hash` (in bytes).
 pub const HASH_LENGTH: usize = 32;
@@ -20,20 +16,20 @@ pub type Hash = [u8; HASH_LENGTH];
 pub fn kv_hash(key: &[u8], value: &[u8]) -> Hash {
     // TODO: result instead of panic
     // TODO: make generic to allow other hashers
-    let mut hasher = Hasher::new();
-    hasher.update(&[0]);
-
-    let key_length = u32::try_from(key.len()).expect("key must be less than 2^32 bytes");
-    hasher.update(&key_length.to_le_bytes());
+    let mut hasher = blake3::Hasher::new();
+    // panics if key is longer than 255!
+    let key_length = u8::try_from(key.len()).expect("key must be less than 256 bytes");
+    hasher.update(&key_length.to_be_bytes());
     hasher.update(key);
 
-    let val_length = u32::try_from(value.len()).expect("value must be less than 2^32 bytes");
-    hasher.update(&val_length.to_le_bytes());
+    // panics if value is longer than 65535!
+    let val_length = u16::try_from(value.len()).expect("value must be less than 65,536 bytes");
+    hasher.update(&val_length.to_be_bytes());
     hasher.update(value);
 
     let res = hasher.finalize();
     let mut hash: Hash = Default::default();
-    hash.copy_from_slice(&res[..]);
+    hash.copy_from_slice(res.as_bytes());
     hash
 }
 
@@ -41,14 +37,13 @@ pub fn kv_hash(key: &[u8], value: &[u8]) -> Hash {
 /// child (if any), and the hash of its right child (if any).
 pub fn node_hash(kv: &Hash, left: &Hash, right: &Hash) -> Hash {
     // TODO: make generic to allow other hashers
-    let mut hasher = Hasher::new();
-    hasher.update(&[1]);
+    let mut hasher = blake3::Hasher::new();
     hasher.update(kv);
     hasher.update(left);
     hasher.update(right);
 
     let res = hasher.finalize();
     let mut hash: Hash = Default::default();
-    hash.copy_from_slice(&res[..]);
+    hash.copy_from_slice(res.as_bytes());
     hash
 }
